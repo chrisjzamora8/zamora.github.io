516de9de1ed60f2447bb8a6edf3beb074ba19972
chrisjzamora8 <46986611+chrisjzamora8@users.noreply.github.com>
2019-04-10 21:41:37 -0500

    update

---
+++ vendingMachine.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace WindowsFormsApp42
+{
+    partial class vendingMachine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(vendingMachine));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lblBoxCola = new System.Windows.Forms.Label();
+            this.lblBoxrootBeer = new System.Windows.Forms.Label();
+            this.lblBoxlemonLime = new System.Windows.Forms.Label();
+            this.lblBoxgrape = new System.Windows.Forms.Label();
+            this.lblBoxcreamSoda = new System.Windows.Forms.Label();
+            this.lblBoxTotal = new System.Windows.Forms.Label();
+            this.exitBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(32, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(66, 64);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(517, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 64);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(32, 161);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(66, 67);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(517, 150);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(60, 67);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(32, 326);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(66, 67);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "$1.00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(594, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "$1.00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "$1.00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(594, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "$1.50";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(104, 326);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "$1.50";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(104, 41);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Drinks Left:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(594, 41);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(110, 25);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Drinks Left:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(104, 203);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(110, 25);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Drinks Left:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(594, 192);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(110, 25);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Drinks Left:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(104, 368);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(110, 25);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Drinks Left:";
+            // 
+            // lblBoxCola
+            // 
+            this.lblBoxCola.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lblBoxCola.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBoxCola.Location = new System.Drawing.Point(220, 9);
+            this.lblBoxCola.Name = "lblBoxCola";
+            this.lblBoxCola.Size = new System.Drawing.Size(180, 70);
+            this.lblBoxCola.TabIndex = 17;
+            // 
+            // lblBoxrootBeer
+            // 
+            this.lblBoxrootBeer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lblBoxrootBeer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBoxrootBeer.Location = new System.Drawing.Point(710, 9);
+            this.lblBoxrootBeer.Name = "lblBoxrootBeer";
+            this.lblBoxrootBeer.Size = new System.Drawing.Size(180, 70);
+            this.lblBoxrootBeer.TabIndex = 18;
+            // 
+            // lblBoxlemonLime
+            // 
+            this.lblBoxlemonLime.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lblBoxlemonLime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBoxlemonLime.Location = new System.Drawing.Point(220, 161);
+            this.lblBoxlemonLime.Name = "lblBoxlemonLime";
+            this.lblBoxlemonLime.Size = new System.Drawing.Size(180, 70);
+            this.lblBoxlemonLime.TabIndex = 19;
+            // 
+            // lblBoxgrape
+            // 
+            this.lblBoxgrape.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lblBoxgrape.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBoxgrape.Location = new System.Drawing.Point(710, 147);
+            this.lblBoxgrape.Name = "lblBoxgrape";
+            this.lblBoxgrape.Size = new System.Drawing.Size(180, 70);
+            this.lblBoxgrape.TabIndex = 20;
+            // 
+            // lblBoxcreamSoda
+            // 
+            this.lblBoxcreamSoda.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lblBoxcreamSoda.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBoxcreamSoda.Location = new System.Drawing.Point(254, 323);
+            this.lblBoxcreamSoda.Name = "lblBoxcreamSoda";
+            this.lblBoxcreamSoda.Size = new System.Drawing.Size(180, 70);
+            this.lblBoxcreamSoda.TabIndex = 21;
+            // 
+            // lblBoxTotal
+            // 
+            this.lblBoxTotal.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.lblBoxTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBoxTotal.Location = new System.Drawing.Point(545, 314);
+            this.lblBoxTotal.Name = "lblBoxTotal";
+            this.lblBoxTotal.Size = new System.Drawing.Size(331, 142);
+            this.lblBoxTotal.TabIndex = 22;
+            this.lblBoxTotal.Text = "Total Sales:";
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(580, 485);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(124, 37);
+            this.exitBtn.TabIndex = 23;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // vendingMachine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 534);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.lblBoxTotal);
+            this.Controls.Add(this.lblBoxcreamSoda);
+            this.Controls.Add(this.lblBoxgrape);
+            this.Controls.Add(this.lblBoxlemonLime);
+            this.Controls.Add(this.lblBoxrootBeer);
+            this.Controls.Add(this.lblBoxCola);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "vendingMachine";
+            this.Text = "Vending Machine:";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lblBoxCola;
+        private System.Windows.Forms.Label lblBoxrootBeer;
+        private System.Windows.Forms.Label lblBoxlemonLime;
+        private System.Windows.Forms.Label lblBoxgrape;
+        private System.Windows.Forms.Label lblBoxcreamSoda;
+        private System.Windows.Forms.Label lblBoxTotal;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
---
+++ vendingMachine.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp42
+{
+    public partial class vendingMachine : Form
+    {
+        // initialize variables to call and connect to the form
+        private int totalQuantity = 20;
+        private double colaCost = 1;
+        private double rootBeerCost = 1;
+        private double lemonLimeCost = 1;
+        private double grapeCost = 1.5;
+        private double creamSodaCost = 1.5;
+        private General objGeneral;
+
+        public vendingMachine()
+        {
+            InitializeComponent();
+            this.objGeneral = new General();
+            this.objGeneral.cola = 20;
+            this.objGeneral.rootBeer = 20;
+            this.objGeneral.lemonLime = 20;
+            this.objGeneral.grape = 20;
+            this.objGeneral.creamSoda = 20;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Image.FromFile(System.AppContext.BaseDirectory + @"\Drink Machine\Cola.bmp");
+            Image.FromFile(System.AppContext.BaseDirectory + @"\Drink Machine\CreamSoda.bmp");
+            Image.FromFile(System.AppContext.BaseDirectory + @"\Drink Machine\GrapeSoda.bmp");
+            Image.FromFile(System.AppContext.BaseDirectory + @"\Drink Machine\LemonLime.bmp");
+            Image.FromFile(System.AppContext.BaseDirectory + @"\Drink Machine\RootBeer.bmp");
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (this.objGeneral.cola == 0)
+            {
+                MessageBox.Show("Sorry Sold Out");
+                return;
+            }
+            this.objGeneral.cola = this.objGeneral.cola - 1;
+            updateMachine();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (this.objGeneral.rootBeer == 0)
+            {
+                MessageBox.Show("Sorry Sold Out");
+                return;
+            }
+            this.objGeneral.rootBeer = this.objGeneral.rootBeer - 1;
+            updateMachine();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (this.objGeneral.lemonlime == 0)
+            {
+                MessageBox.Show("Sorry Sold Out");
+                return;
+            }
+            this.objGeneral.lemonlime = this.objGeneral.lemonlime - 1;
+            updateMachine();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if (this.objGeneral.grape == 0)
+            {
+                MessageBox.Show("Sorry Sold Out");
+                return;
+            }
+            this.objGeneral.grape = this.objGeneral.grape - 1;
+            updateMachine();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            if (this.objGeneral.creamSoda == 0)
+            {
+                MessageBox.Show("Sorry Sold Out");
+                return;
+            }
+            this.objGeneral.creamSoda = this.objGeneral.creamSoda - 1;
+            updateMachine();
+        }
+        private void updateMachine()
+        {
+            lblBoxCola.Text = this.objGeneral.Cola.ToString();
+            lblBoxrootBeer.Text = this.objGeneral.rootBeer.ToString();
+            lblBoxgrape.Text=this.objGeneral.grape.ToString();
+            lblBoxlemonLime.Text = this.objGeneral.lemonLime.ToString();
+            lblBoxcreamSoda.Text = this.objGeneral.creamSoda.ToString();
+
+            double cola = (totalQuantity - this.objGeneral.Cola) * colaCost;
+            double rootBeer = (totalQuantity - this.objGeneral.rootBeer) * rootBeerCost;
+            double lemonLime = (totalQuantity - this.objGeneral.lemonLime) * lemonLimeCost;
+            double grapeSoda = (totalQuantity - this.objGeneral.grape) * grapeCost;
+            double creamSoda  = (totalQuantity - this.objGeneral.creamSoda) *creamSodaCost;
+
+            lblBoxTotal.Text = "$" + Math.Round((cola + rootBeer + lemonLime + grapeSoda + creamSoda), 2).ToString();
+        }
+        }
+}
